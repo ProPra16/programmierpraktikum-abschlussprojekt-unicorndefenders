084a9bd48857bea325e534c8b4c6d02414b62fe3
Alessa <alessandra.wiechers@uni-duesseldorf.de>
2016-06-29 19:54:19 +0200

    ADD: Click Events

--- src/de/hhu/propra16/unicorndefenders/tddt/Controller.java
+++ src/de/hhu/propra16/unicorndefenders/tddt/Controller.java
@@ -1,18 +1,55 @@
 package de.hhu.propra16.unicorndefenders.tddt;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 
-public class Controller {
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
 
     @FXML
     TextArea codeArea;
     @FXML
     TextArea testArea;
+    @FXML
+    TableView<MenuEntry> taskMenu;
+    ObservableList<MenuEntry> taskMenuData = FXCollections.observableArrayList();
+
 
 
     // Dummy-Methoden
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        taskMenuData.add(new MenuEntry("A1", "Ja"));
+        taskMenuData.add(new MenuEntry("A2", "Nein"));
+        taskMenu.setItems(taskMenuData);
+
+        taskMenu.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if (event.getButton() == MouseButton.PRIMARY){
+                    MenuEntry selectedEntry = taskMenu.getSelectionModel().getSelectedItem();
+                    System.out.println(selectedEntry.getTaskTitle());
+                }
+            }
+        });
+
+
+
+
+    }
 
     public void loadCatalog(){
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -29,9 +66,6 @@ public class Controller {
         codeArea.setEditable(false);
     }
 
-    public void click(){
-
-    }
 
 
 }
