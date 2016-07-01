72a18ea3ac2d4f807f8d333023e9c8d6cc48740f
eyoez100 <eyyuep.oezen@uni-duesseldorf.de>
2016-07-01 19:04:50 +0200

    ADD: Stoppuhr-Klasse für das Tracking

---
+++ src/de/hhu/propra16/unicorndefenders/tddt/StoppUhr.java
@@ -0,0 +1,23 @@
+
+public class StoppUhr{
+
+   public static long timeStart=0;     // Startzeit
+   public static long timeEnd=0;       // Endzeit
+
+   public static void starten(){
+      timeStart = System.currentTimeMillis();
+   }  // misst Startzeit
+
+   public static void beenden() {
+      timeEnd = System.currentTimeMillis();
+   }    // misst Endzeit
+
+   public static long zeit(){             // misst die Zeit zwischen Start- und Endzeit
+      long time=0;
+      if(timeStart!=0&&timeEnd!=0){
+         time=timeEnd-timeStart;
+      }
+
+      return time/1000;    //Ausgabe in Sekunden
+   }
+}
