9a5f9edfd3acc5d2e78ee1dcfd4b8ee906631010
Geralt <sebastian.neudek@uni-duesseldorf.de>
2016-07-10 23:45:12 +0200

    Different Colors
    Das Pie-Chart besteht jetzt aus mehreren Farben

--- src/de/hhu/propra16/unicorndefenders/tddt/Analyser.java
+++ src/de/hhu/propra16/unicorndefenders/tddt/Analyser.java
@@ -4,25 +4,29 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.ArcType;
 import javafx.stage.Stage;
+
 import java.util.List;
 
 public class Analyser {
 
-    public Canvas analyse(List<Integer> Zeiten) throws Exception{
-        int gesamtzeit=0;
-        for (int i: Zeiten) {
-            gesamtzeit +=i;
-        }
-        Group root = new Group();
-        Canvas canvas = new Canvas(400, 300);
-        int temp =0;
-        for (int i: Zeiten) {
-            canvas.getGraphicsContext2D().setFill(Color.GREEN);
-            canvas.getGraphicsContext2D().fillArc(200, 200, 100, 100, 360*temp/gesamtzeit, 360*i/gesamtzeit, ArcType.ROUND);
-            temp+=i;
-        }
-        root.getChildren().add(canvas);
-        return canvas;
-    }
+   public Canvas analyse(List<Integer> Zeiten) throws Exception {
+      Color[] colors = {Color.BLUE, Color.GREEN, Color.DARKGRAY, Color.ORANGE, Color.RED, Color.DARKORANGE, Color.YELLOW, Color.AQUAMARINE, Color.DARKGOLDENROD, Color.LIME};
+      int gesamtzeit = 0;
+      int j =0;
+      for (int i : Zeiten) {
+         gesamtzeit += i;
+      }
+      Group root = new Group();
+      Canvas canvas = new Canvas(400, 300);
+      int temp = 0;
+      for (int i : Zeiten) {
+         canvas.getGraphicsContext2D().setFill(colors[j%10]);
+         canvas.getGraphicsContext2D().fillArc(200, 200, 100, 100, 360 * temp / gesamtzeit, 360 * i / gesamtzeit, ArcType.ROUND);
+         temp += i;
+         j++;
+      }
+      root.getChildren().add(canvas);
+      return canvas;
+   }
 
 }
