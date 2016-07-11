da0e0e4032ea9fa547a02ef6106c90751cae81c8
Sebastian <sebastian.neudek@uni-duesseldorf.de>
2016-07-11 19:35:37 +0200

    MODIFY: Implementierung der Stage
    Die Implementierung der Stage wurde eingefügt.

--- src/de/hhu/propra16/unicorndefenders/tddt/TrackingResultWindow.java
+++ src/de/hhu/propra16/unicorndefenders/tddt/TrackingResultWindow.java
@@ -1,16 +1,27 @@
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Sebastian on 04.07.2016.
  */
 public class TrackingResultWindow extends Stage {
 
-   public TrackingResultWindow(){
+   public TrackingResultWindow(ArrayList<Integer> Tracking_List) {
       super();
 
       setTitle("Tracking Result");
-
-
+      Group root = new Group();
+      try {
+         root.getChildren().add(Analyser.analyse(Tracking_List));
+      } catch (Exception e) {
+         System.out.println("Fehler");
+      }
+      Scene scene = new Scene(root);
+      setScene(scene);
    }
 
 
